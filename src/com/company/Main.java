a5b6389b1f2234d23ea31ed42fe7cd60bc94a922
solov <solovejko00@mail.ru>
2021-06-12 20:37:43 +0300

    Using threads

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,14 +6,23 @@ import java.time.*;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        // https://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=0b5edc7455a336d544760ce639198bc9&units=metric&lang=ru
+    public static void main(String[] args) throws IOException{
+        for (int i = 0; i < 200; i++){
+            MyThread thread = new MyThread();
+            thread.start();
+        }
 
-        URL url = new URL("http://localhost:3000/weather/city?q=Moscow");
+        try {
+            spam(10);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void sendRequest(URL url) throws IOException {
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
 
         con.setRequestMethod("GET");
-
         con.connect();
 
         Instant firstPoint = Instant.now();
@@ -21,23 +30,35 @@ public class Main {
         Instant secondPoint = Instant.now();
 
         long delay = Duration.between(firstPoint, secondPoint).toMillis();
-        System.out.println(delay);
+        StringBuilder urlString = new StringBuilder();
 
         if (Code == HttpURLConnection.HTTP_OK){
             BufferedReader input = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            StringBuilder urlString = new StringBuilder();
             String line;
-
             while((line = input.readLine()) != null) {
                 urlString.append(line);
-                urlString.append("\n");
             }
-
             input.close();
-
-            System.out.println(urlString.toString());
         }
 
+        System.out.println(delay + " " + urlString.toString());
         con.disconnect();
     }
+
+    static class MyThread extends Thread {
+        @Override
+        public void run(){
+            try {
+                spam(10);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void spam(int n) throws IOException {
+        for (int i = 0; i < n; i++)
+            sendRequest(new URL("http://localhost:3000/weather/city?q=Moscow"));
+    }
+
 }
