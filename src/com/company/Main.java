03b08d73ef83c324aa0fc0463eb7dd93dd6b2b03
solov <solovejko00@mail.ru>
2021-06-12 17:51:44 +0300

    Get request and check delay

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,25 +2,42 @@ package com.company;
 
 import java.net.*;
 import java.io.*;
-
+import java.time.*;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=0b5edc7455a336d544760ce639198bc9&units=metric&lang=ru");
-        URLConnection urlConnect = url.openConnection();
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // https://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=0b5edc7455a336d544760ce639198bc9&units=metric&lang=ru
 
-        if (urlConnect.getContentLength() > 0) {
-            BufferedReader input = new BufferedReader(new InputStreamReader(urlConnect.getInputStream()));
-            String urlString = "";
-            String current;
+        URL url = new URL("http://localhost:3000/weather/city?q=Moscow");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
 
-            while((current = input.readLine()) != null) {
-                urlString += current;
+        con.setRequestMethod("GET");
+
+        con.connect();
+
+        Instant firstPoint = Instant.now();
+        int Code = con.getResponseCode();
+        Instant secondPoint = Instant.now();
+
+        long delay = Duration.between(firstPoint, secondPoint).toMillis();
+        System.out.println(delay);
+
+        if (Code == HttpURLConnection.HTTP_OK){
+            BufferedReader input = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            StringBuilder urlString = new StringBuilder();
+            String line;
+
+            while((line = input.readLine()) != null) {
+                urlString.append(line);
+                urlString.append("\n");
             }
 
             input.close();
 
-            System.out.println(urlString);
+            System.out.println(urlString.toString());
         }
+
+        con.disconnect();
     }
 }
